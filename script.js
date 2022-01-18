3410dc5b5b96becdd08bc01e31b7f9b52b96d717
Akshansh Shrivastava <Akshansh773@gmail.com>
2022-01-18 23:04:15 +0530

    Complete Project commit

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 var hamber = document.querySelector(".hamber-icon");
 var baseNav = document.querySelector(".base-nav");
 var close = document.querySelector(".close-icon");
+var navopen = document.querySelector(".nav-open");
+var navItems = document.querySelectorAll(".nav-list-items");
+
+console.log(navopen);
+for (var i = 0; i < navItems.length; i++) {
+  navItems[i].addEventListener("click", () => {
+    baseNav.classList.remove("nav-open");
+  });
+}
+
 hamber.addEventListener("click", () => {
   baseNav.classList.add("nav-open");
- 
 });
 
 close.addEventListener("click", () => {
